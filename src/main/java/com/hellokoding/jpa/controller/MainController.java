73f522966ef7f0309a2b7554eef78387f42873dd
nik <lnalna@yandex.ru>
2018-09-29 17:21:46 +0300

    add swagger

--- src/main/java/com/hellokoding/jpa/controller/MainController.java
+++ src/main/java/com/hellokoding/jpa/controller/MainController.java
@@ -2,16 +2,16 @@ package com.hellokoding.jpa.controller;
 
 import com.hellokoding.jpa.HelloJpaApplication;
 import com.hellokoding.jpa.model.Book;
-import com.hellokoding.jpa.model.BookDetail;
 import com.hellokoding.jpa.repository.BookRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/v1/rest")
 public class MainController {
 
     private final Logger logger = LoggerFactory.getLogger(HelloJpaApplication.class);
@@ -24,10 +24,14 @@ public class MainController {
         return "Spring Boot 2.0.5 Application is working!";
     }
 
-    @GetMapping("/save/book/{title}/{pages}")
-    public Book save(@PathVariable(value = "title") String title,
-                     @PathVariable(value = "pages") Integer pages) {
+    @PostMapping("/save/book")
+    public Book save(@RequestBody Book book) {
         logger.info("MainController.save: save book");
-        return bookRepository.save(new Book(title, new BookDetail(pages)));
+        return bookRepository.save(book);
+    }
+
+    @GetMapping("/all")
+    public List<Book> retrieveAll() {
+        return bookRepository.findAll();
     }
 }
